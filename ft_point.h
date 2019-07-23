f2b401912b9a7b532ce5cb20338e3ff9e10051cc
cairumeng <45143617+cairumeng@users.noreply.github.com>
2019-07-23 23:34:43 +0200

    Update ft_point.h

--- ft_point.h
+++ ft_point.h
@@ -1,7 +1,12 @@
-typedef struct s_point
+#ifndef FT_POINT_H
+#define FT_POINT_H
+
+typedef struct  s_point
 {
-	int x;
-	int y;
-} t_point;
+    int x;
+    int y;
+}               t_point;
 
+void            set_point(t_point *point);
 
+#endif
